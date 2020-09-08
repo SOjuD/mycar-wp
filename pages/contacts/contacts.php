404c2a9e824ac7c0c1b4ce35bc8fd369ab242414
Oleg <talrash666@gmail.com>
2020-09-08 11:19:34 +0300

    add base markup contacts page

--- pages/contacts/contacts.php
+++ pages/contacts/contacts.php
@@ -1,2 +1,14 @@
 <?php get_header(); ?>
+<main>
+    <section class="contactsPage">
+        <div class="container">
+            <div class="secTitle">Контакты</div>
+            <div class="row">
+                <div class="col-12 col-md-5 col-lg-4 col-xl-3">
+
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
 <?php get_footer(); ?>
